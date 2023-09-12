48fd98d666a136c0a95212226ca2d0c6653bb24c
Ericson-WebDeveloper <60337201+Ericson-WebDeveloper@users.noreply.github.com>
2023-09-12 14:42:23 +0800

    Update PaymentDone.tsx

--- react-techwatch/src/pages/paypal/PaymentDone.tsx
+++ react-techwatch/src/pages/paypal/PaymentDone.tsx
@@ -117,9 +117,7 @@ const PaymentDone = (props: PaymentDoneProps) => {
                         :
                         null
                     }
-                  {/* <button className='w-[150px] mx-auto px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded
-                        shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
-                        active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out'>Track Page</button> */}
+
                 </div>
             </div>
             :
