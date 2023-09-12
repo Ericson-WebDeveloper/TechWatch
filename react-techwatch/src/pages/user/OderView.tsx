cf6682877de68e99d1104cbbbcbb90a8cf6e3867
Ericson-WebDeveloper <60337201+Ericson-WebDeveloper@users.noreply.github.com>
2023-09-12 14:42:55 +0800

    Update OderView.tsx

--- react-techwatch/src/pages/user/OderView.tsx
+++ react-techwatch/src/pages/user/OderView.tsx
@@ -57,8 +57,7 @@ const OderView = ({children}: OderViewProps) => {
                                 null
                             }
                             
-                            {/* <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel natus deserunt, itaque esse, blanditiis repellat, explicabo et placeat dolorum 
-                                optio perspiciatis doloribus velit! Quidem aut architecto nisi libero, incidunt ipsam.</p> */}
+                   
                         </div>
                         {
                             data?.data?.items?.map((item: itemOrder, index:number) => {
@@ -76,12 +75,7 @@ const OderView = ({children}: OderViewProps) => {
                                                 <p>{item.qty}</p>
                                             </div>
                                             <div className='jusify-center items-center'>
-                                            {/* <Link to={`/user/order-tracking/${1}`}>
-                                            <button type="button" className="w-full px-6 py-2.5
-                                                bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md
-                                                hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
-                                                active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Tracking</button>
-                                                </Link> */}
+                         
                                             </div>
                                         </div>  
                             })
