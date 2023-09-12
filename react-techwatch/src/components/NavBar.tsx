d5e0d227bf5d66ba7f29c42b783cc60e5e3d28d6
Ericson-WebDeveloper <60337201+Ericson-WebDeveloper@users.noreply.github.com>
2023-09-12 14:46:47 +0800

    Update NavBar.tsx

--- react-techwatch/src/components/NavBar.tsx
+++ react-techwatch/src/components/NavBar.tsx
@@ -128,10 +128,6 @@ const NavBar = (props: NavBarProps) => {
                             null
                         }
                         
-                        {/* <li>
-                            <Link className="dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100"
-                            to="/">Something else here</Link>
-                        </li> */}
                     </ul>
                 </div>
                 {
