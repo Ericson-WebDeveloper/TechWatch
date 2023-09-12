d63353ab66c98b7a6d5562c01dab9bd8423101ea
Ericson-WebDeveloper <60337201+Ericson-WebDeveloper@users.noreply.github.com>
2023-09-12 14:45:23 +0800

    Update Cart.ts

--- react-techwatch/src/models/Cart.ts
+++ react-techwatch/src/models/Cart.ts
@@ -1,11 +1,4 @@
 export interface cartinterface {
-    // uuid: uuid.v4(),
-    // id: payload.id,
-    // name: payload.name,
-    // price: parseFloat(payload.price),
-    // totalprice: parseFloat(payload.price) * 1,
-    // img: payload.img,
-    // qty: 1
     uuid: string;
     id: number;
     name: string;
